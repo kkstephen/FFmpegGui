7b880ba325b2d76800852d5c091ecf2aa2b03169
Stephen Yeung <ttshpyeung@hotmail.com>
2021-01-14 16:34:29 +0800

    update profile

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
-using System.Threading.Tasks;
-using System.Windows; 
-using Microsoft.Win32;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Documents;
 
 namespace FFGui
 {
@@ -23,13 +23,21 @@ namespace FFGui
         {
             OpenFileDialog dialog = new OpenFileDialog();
 
-            if (dialog.ShowDialog() == true)
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
                 this.txt_fileIn.Text = dialog.FileName;
+                this.txt_fileOut.Text = System.IO.Path.GetDirectoryName(dialog.FileName) + "\\" + GetFileName(dialog.FileName, this.chk_format.IsChecked);
+            }
+        }
 
-                string tmp = System.IO.Path.GetExtension(dialog.FileName);
+        private void Btn_out_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
 
-                this.txt_fileOut.Text = dialog.FileName.Replace(tmp, "_out.mp4");
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string fname = GetFileName(this.txt_fileIn.Text, this.chk_format.IsChecked);
+                this.txt_fileOut.Text = dialog.SelectedPath + "\\" + fname;
             }
         }
 
@@ -49,7 +57,8 @@ namespace FFGui
                 str += " -max_muxing_queue_size 1024 ";
             }
 
-            if (this.chk_vcopy.IsChecked == true)
+            //video codec
+            if (this.cb_video.Text == "Auto" && this.cb_vrate.Text == "Auto")
             {
                 str += " -vcodec copy";
             }
@@ -64,19 +73,38 @@ namespace FFGui
                 {
                     str += " -b:v " + this.cb_vrate.SelectedValue;
                 }
+
+                str += " -bufsize " + this.cb_vrate.SelectedValue;
             }
 
+            //8bit only
             if (this.bit8only.IsChecked == true)
             {
                 str += " -vf format=yuv420p";
             }
 
+            //1080i compensate
             if (this.chk_deinter.IsChecked == true)
             {
                 str += " -deinterlace";
             }
+
+            //profile level
+            if (this.cb_profile.Text != "Auto")
+            {
+                switch(this.cb_profile.SelectedIndex)
+                {
+                    case 1:
+                        str += " -profile:v main -level:v 4";
+                        break;
+                    case 2: str += " -profile:v high -level:v 4.2";
+                        break;
+                    default:                       
+                        break;
+                }
+            }
             
-            if (this.chk_acopy.IsChecked == true)
+            if (this.cb_audio.Text == "Auto" && this.cb_arate.Text == "Auto")
             {
                 str += " -acodec copy ";
             }
@@ -109,7 +137,6 @@ namespace FFGui
             }
 
             //subclip
-
             int s = 0;
 
             if (this.txt_start.Text != "0")
@@ -132,12 +159,19 @@ namespace FFGui
                     }
                     else
                     {
-                        MessageBox.Show("Time factor number is worng!");
+                        System.Windows.Forms.MessageBox.Show("Time factor number is worng!");
                     }
                 }             
             }
 
-            str += " \"" + this.txt_fileOut.Text + "\"";
+            string output = this.txt_fileOut.Text;
+
+            if (string.IsNullOrEmpty(this.txt_fileOut.Text))
+            {
+                output = System.IO.Path.GetDirectoryName(this.txt_fileIn.Text) + "\\" + GetFileName(this.txt_fileIn.Text, this.chk_format.IsChecked);
+            }
+
+            str += " \"" + output + "\"";
 
             this.txt_cmd.Document.Blocks.Clear();
             this.txt_cmd.AppendText(str);
@@ -146,6 +180,26 @@ namespace FFGui
         private void Btn_clear_Click(object sender, RoutedEventArgs e)
         {
             this.txt_cmd.Document.Blocks.Clear();
+        }
+        
+        private static string GetFileName(string file, bool? mp4_fm)
+        {
+            string tmp = System.IO.Path.GetFileNameWithoutExtension(file);
+            string ext = System.IO.Path.GetExtension(file);
+
+            if (mp4_fm != null && mp4_fm == true)
+            {
+                ext = ".mp4";
+            }
+
+            return tmp + "_out" + ext;
+        }
+
+        private void Btn_copy_Click(object sender, RoutedEventArgs e)
+        {
+            TextRange doc = new TextRange(this.txt_cmd.Document.ContentStart, this.txt_cmd.Document.ContentEnd);
+
+            System.Windows.Clipboard.SetDataObject(doc.Text);
         }
     }
 }
